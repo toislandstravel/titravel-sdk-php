4a87fba4d871b8934cabf07dd8c8def7a08f3b01
mbman <mario.baricevic@gmail.com>
2014-01-02 16:24:33 +0100

    Titravel username & password credentials

---
+++ lib/TiTravel/Auth/Credentials.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace TiTravel\Auth;
+
+/**
+ * Titravel API's username & password credentials
+ *
+ */
+class Credentials {
+
+    /**
+     * Client b2b ID id obtained from To Islands Travel
+     * @var int
+     */
+    private $b2b;
+
+    /**
+     * Client API access code obtained from To Islands Travel
+     * @var string
+     */
+    private $code;
+
+    /**
+     * @param int $b2b  Client b2b ID id obtained from To Islands Travel
+     * @param [type] $code Client API access code obtained from To Islands Travel
+     */
+    public function __construct($b2b, $code)
+    {
+        $this->b2b = $b2b;
+        $this->code = $code;
+    }
+
+    /**
+     * Returns the code
+     * @return string Client API authorization code
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Returns the B2B client id
+     * @return string Client B2B id
+     */
+    public function getB2B()
+    {
+        return $this->b2b;
+    }
+}
