352f2ee57b71d42761dee9f4f4cdc36ba21718b2
mbman <mario.baricevic@gmail.com>
2014-01-09 10:00:50 +0100

    Updated API endpoint URL

--- lib/TiTravel/Constants.php
+++ lib/TiTravel/Constants.php
@@ -6,5 +6,5 @@ class Constants
     const SDK_NAME = 'sdk-php';
     const SDK_VERSION = '0.0.1';
 
-    const API_LIVE_ENDPOINT = 'http://b2b.titravel.eu/?a=xml_api&v=2&format=json';
+    const API_LIVE_ENDPOINT = 'http://www.adrianet.org/?a=xml_api&v=2&format=json';
 }
