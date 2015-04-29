80f98b9342544aa81506a23ec0d3dc348b193b82
mbman <mario.baricevic@gmail.com>
2015-04-29 21:28:07 +0200

    Version 0.2.0

--- lib/TiTravel/Constants.php
+++ lib/TiTravel/Constants.php
@@ -4,7 +4,7 @@ namespace TiTravel;
 class Constants
 {
     const SDK_NAME = 'sdk-php';
-    const SDK_VERSION = '0.0.1';
+    const SDK_VERSION = '0.2.0';
 
     const API_LIVE_ENDPOINT = 'https://www.adrianet.org/?a=xml_api&v=4&format=xml';
 }
